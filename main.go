2133efe2178f4578b8aab260591c74246a469306
ryoukata <ryoya3091@gmail.com>
2021-05-15 16:58:30 +0900

    feat(#2): add function to receive message from NSQ.

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"sync"
+
+	"github.com/nsqio/go-nsq"
+	"gopkg.in/mgo.v2"
 )
 
 var fatalErr error
@@ -20,4 +25,44 @@ func main() {
 			os.Exit(1)
 		}
 	}()
+
+	log.Println("Connect to DataBase...")
+	// TODO：コンテナ名に修正すること
+	db, err := mgo.Dial("localhost")
+	if err != nil {
+		fatal(err)
+		return
+	}
+	defer func() {
+		log.Println("Close Connection for DataBase...")
+		db.Close()
+	}()
+	pollData := db.DB("ballots").C("polls")
+
+	var countsLock sync.Mutex
+	var counts map[string]int
+	log.Println("Connect to NSQ...")
+	// Set up Object to observe NSQ votes Topic.
+	q, err := nsq.NewConsumer("votes", "counter", nsq.NewConfig())
+	if err != nil {
+		fatal(err)
+		return
+	}
+
+	q.AddHandler(nsq.HandlerFunc(func(m *nsq.Message) error {
+		countsLock.Lock()
+		defer countsLock.Unlock()
+		if counts == nil {
+			counts = make(map[string]int)
+		}
+		vote := string(m.Body)
+		counts[vote]++
+		return nil
+	}))
+
+	// TODO：ホストをコンテナ名に修正すること
+	if err := q.ConnectToNSQLookupd("localhost:4161"); err != nil {
+		fatal(err)
+		return
+	}
 }
